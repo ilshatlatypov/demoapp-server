76d4fb5d34d71ce9d38f4251a4eb088577c327bb
Ilshat Latypov <latypov.ilshat@gmail.com>
2016-09-28 08:31:10 +0300

    users search by firstname and lastname

--- src/main/java/ru/jvdev/demoapp/server/repository/UserRepository.java
+++ src/main/java/ru/jvdev/demoapp/server/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ru.jvdev.demoapp.server.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import static ru.jvdev.demoapp.server.utils.RequestParamNames.USERNAME;
@@ -13,4 +16,9 @@ import ru.jvdev.demoapp.server.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(@Param(USERNAME) String username);
+
+    @RestResource(path = "findByNamesStartingWith", rel = "findByNamesStartingWith")
+    Page<User> findByFirstnameStartingWithOrLastnameStartingWith(@Param("firstname") String firstname,
+                                                                 @Param("lastname") String lastname,
+                                                                 Pageable p);
 }
