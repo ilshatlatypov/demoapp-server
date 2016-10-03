be68f979d05942d988f5c163808c93a82c686a45
Ilshat Latypov <latypov.ilshat@gmail.com>
2016-10-03 15:58:12 +0300

    add etag and lastmodified headers to entity responses

--- src/main/java/ru/jvdev/demoapp/server/entity/Task.java
+++ src/main/java/ru/jvdev/demoapp/server/entity/Task.java
@@ -1,30 +1,42 @@
 package ru.jvdev.demoapp.server.entity;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Created by ilshat on 04.08.16.
  */
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Data
+@ToString(exclude = {"user", "version", "lastModified"})
 @EqualsAndHashCode(exclude = "user")
 public class Task {
 
@@ -41,6 +53,17 @@ public class Task {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Version
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Long version;
+    @LastModifiedDate
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Date lastModified;
 
     // required for JPA
     public Task() {
--- src/main/java/ru/jvdev/demoapp/server/entity/User.java
+++ src/main/java/ru/jvdev/demoapp/server/entity/User.java
@@ -1,29 +1,38 @@
 package ru.jvdev.demoapp.server.entity;
 
+import java.util.Date;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Data
-@ToString(exclude = "password")
+@ToString(exclude = {"tasks", "password", "version", "lastModified"})
 @EqualsAndHashCode(exclude = "tasks")
 public class User {
 
@@ -46,6 +55,17 @@ public class User {
     private Role role;
     @OneToMany(mappedBy = "user")
     private Set<Task> tasks;
+
+    @Version
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Long version;
+    @LastModifiedDate
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Date lastModified;
 
     // required for JPA
     public User() {}
